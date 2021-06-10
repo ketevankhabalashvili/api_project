40121dce19c546fc3104b99a8fcfbc6a448100f0
ketevankhabalashvili <khabalashviliketevan@gmail.com>
2021-06-10 22:43:49 +0400

    initial commit

---
+++ database/migrations/2021_06_10_181616_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('sender_user_id')->nullable();
+            $table->string('recipient_user_id')->nullable();
+            $table->decimal('amount', 7,2)->nullable();
+            $table->decimal('commission_amount', 7,2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
